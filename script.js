18b3aa7757e2479b14b6f23b496234be0d16bb15
rockypatel2613 <patelnilendra809@gmail.com>
2023-03-13 00:18:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// https://github.com/Rustcodeweb/Cursor-Animation-With-Hover-Effect-Using-GSAP
+
+let mouseCursor = document.querySelector(".cursor");
+let btn = document.querySelectorAll("button");
+
+window.addEventListener("mousemove", cursor);
+
+function cursor(cursorPointer){
+  gsap.to(mouseCursor, 0.4, {
+    x: cursorPointer.clientX,
+    y: cursorPointer.clientY,
+  });
+}
+
+btn.forEach(function(link) {
+  link.addEventListener("mouseover", function(){
+    mouseCursor.classList.add("scale-cursor");
+    gsap.to(mouseCursor, 0.4, {
+      scale: 5
+    });
+  });
+
+  link.addEventListener("mouseleave", function(){
+    mouseCursor.classList.remove("scale-cursor");
+    gsap.to(mouseCursor, 0.4, {
+      scale: 1
+    });
+  });
+});
+
+
